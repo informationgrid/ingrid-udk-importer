ad1c02dd18752bfcd37b3795e62efb556ded3b68
mbenz <mbenz@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2008-08-14 14:32:42 +0000

    - Updated SNSSpatialTypeStrategy to ignore 'free' entries. Only SNS topics need a topicType.

--- src/main/java/de/ingrid/importer/udk/strategy/IDCSNSSpatialTypeStrategy.java
+++ src/main/java/de/ingrid/importer/udk/strategy/IDCSNSSpatialTypeStrategy.java
@@ -51,7 +51,8 @@ public class IDCSNSSpatialTypeStrategy extends IDCStrategyDefault {
 //		String sql = "select id, nativekey from spatial_ref_value;";
 		// Extended query so we can update the index
 		String sql = "select distinct objNode.id as objNodeId, objNode.obj_id as objWorkId, obj.id as objId, " +
-		"spatialRefVal.id as spatialRefValId, spatialRefVal.nativekey as spatialRefNativekey, spatialRef.id as spatialRefId " +
+		"spatialRefVal.id as spatialRefValId, spatialRefVal.nativekey as spatialRefNativekey, spatialRefVal.type as spatialRefValType, " +
+		"spatialRef.id as spatialRefId " +
 		"from spatial_ref_value spatialRefVal, spatial_reference spatialRef, t01_object obj, object_node objNode " +
 		"where spatialRefVal.id = spatialRef.spatial_ref_id " +
 		"and spatialRef.obj_id = obj.id " +
@@ -68,10 +69,11 @@ public class IDCSNSSpatialTypeStrategy extends IDCStrategyDefault {
 			long id = rs.getLong("spatialRefValId");
 			long spatialRefId = rs.getLong("spatialRefId");
 			String nativeKey = rs.getString("spatialRefNativekey");
+			boolean isSNSTopic = rs.getString("spatialRefValType").equalsIgnoreCase("G");
 			String type = "";
 
 			// convert & write values if not written yet !
-			if (!processedSpatialRefIds.containsKey(spatialRefId)) {
+			if (isSNSTopic && !processedSpatialRefIds.containsKey(spatialRefId)) {
 				// Extract the topic type from the native key.
 				// This is problematic since the ags/rs native keys are NOT unique!
 				if (nativeKey.endsWith("00000000")) {
