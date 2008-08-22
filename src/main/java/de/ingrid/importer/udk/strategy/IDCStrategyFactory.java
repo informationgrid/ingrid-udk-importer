c1775d2dd0eafa94b37cbb1c88d79f0485502d7c
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2008-08-22 10:16:24 +0000

    importer: - implemented updater fixing buggy initial imports (bw, sh), see http://88.198.11.89/jira/browse/INGRIDII-275, http://88.198.11.89/jira/browse/INGRIDII-276

--- src/main/java/de/ingrid/importer/udk/strategy/IDCStrategyFactory.java
+++ src/main/java/de/ingrid/importer/udk/strategy/IDCStrategyFactory.java
@@ -26,10 +26,12 @@ public class IDCStrategyFactory {
 			return new IDCInitDBStrategy1_0_2();
 		} else if (idcVersion.equals("1.0.2_help")) {
 			return new IDCHelpImporterStrategy();
-		} else if (idcVersion.equals("1.0.2_sns_spatial_type")) {
-			return new IDCSNSSpatialTypeStrategy();
 		} else if (idcVersion.equals(IDCStrategy.VALUE_IDC_VERSION_102)) {
 			return new IDCStrategy1_0_2();
+		} else if (idcVersion.equals("1.0.2_fix_import")) {
+			return new IDCImporterFixStrategy();
+		} else if (idcVersion.equals(IDCStrategy.VALUE_IDC_VERSION_102_SNS_SPATIAL_TYPE)) {
+			return new IDCSNSSpatialTypeStrategy();
 		} else if (idcVersion.equals(IDCStrategy.VALUE_IDC_VERSION_103)) {
 			return new IDCStrategy1_0_3();
 		} else {
