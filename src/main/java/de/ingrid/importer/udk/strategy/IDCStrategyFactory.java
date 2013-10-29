acb2afb4f8b78baa8391af926fa12deee8e53a2f
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2013-10-29 14:48:30 +0000

    udk-importer: - Merged Rev. 22831 from [BRANCH-3.3.x] to trunk -> new strategy migrating "open data" from additional fields (HH) to new Open Data checkbox and categories, see https://redmine.wemove.com/issues/245

--- src/main/java/de/ingrid/importer/udk/strategy/IDCStrategyFactory.java
+++ src/main/java/de/ingrid/importer/udk/strategy/IDCStrategyFactory.java
@@ -51,6 +51,7 @@ import de.ingrid.importer.udk.strategy.v331.IDCStrategy3_3_1_RELEASE;
 import de.ingrid.importer.udk.strategy.v331.IDCStrategy3_3_1_a;
 import de.ingrid.importer.udk.strategy.v331.IDCStrategy3_3_1_b;
 import de.ingrid.importer.udk.strategy.v331.IDCStrategy3_3_1_c;
+import de.ingrid.importer.udk.strategy.v331.IDCStrategy3_3_1_d;
 import de.ingrid.importer.udk.strategy.v331.IDCStrategy3_3_1_fixOrigId;
 import de.ingrid.importer.udk.strategy.v34.IDCStrategy3_4_0_RELEASE;
 import de.ingrid.importer.udk.strategy.v34.IDCStrategy3_4_0_a;
@@ -151,6 +152,8 @@ public class IDCStrategyFactory {
             return new IDCStrategy3_3_1_b();
         } else if (idcVersion.equals(IDCStrategy.VALUE_IDC_VERSION_3_3_1_c)) {
             return new IDCStrategy3_3_1_c();
+        } else if (idcVersion.equals(IDCStrategy.VALUE_IDC_VERSION_3_3_1_d)) {
+            return new IDCStrategy3_3_1_d();
         } else if (idcVersion.equals(IDCStrategy.VALUE_IDC_VERSION_3_3_1_RELEASE)) {
             return new IDCStrategy3_3_1_RELEASE();
         } else if (idcVersion.equals(IDCStrategy.VALUE_IDC_VERSION_3_4_0_a)) {
