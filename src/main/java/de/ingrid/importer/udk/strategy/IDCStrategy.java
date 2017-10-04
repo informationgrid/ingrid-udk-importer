8d62fe55f81444c7e2662533ca84982938fa6cd8
Martin Maidhof <martin@wemove.com>
2017-10-04 17:08:37 +0200

    Prepare Release 4.1.0 -> release strategy, changes

--- src/main/java/de/ingrid/importer/udk/strategy/IDCStrategy.java
+++ src/main/java/de/ingrid/importer/udk/strategy/IDCStrategy.java
@@ -187,9 +187,11 @@ public interface IDCStrategy {
     static String VALUE_IDC_VERSION_4_0_3_RELEASE = "4.0.3";
     static String VALUE_IDC_VERSION_4_0_3_fixKeywordsAdVMIS = "4.0.3_fixKeywordsAdVMIS";
 
-    /** InGrid 4.0.4 */
+    /** InGrid 4.1.0 */
+    // 4.0.4 never released ! we just keep these strategies in v404 package !
     static String VALUE_IDC_VERSION_4_0_4_a = "4.0.4_a";
     static String VALUE_IDC_VERSION_4_0_4_b = "4.0.4_b";
+    static String VALUE_IDC_VERSION_4_1_0_RELEASE = "4.1.0";
 
 	/** Order of strategies to execute to obtain most recent IGC.
 	 * Contains all according versions/strategies in ascending order.
@@ -281,6 +283,7 @@ public interface IDCStrategy {
         
         VALUE_IDC_VERSION_4_0_4_a,
         VALUE_IDC_VERSION_4_0_4_b,
+        VALUE_IDC_VERSION_4_1_0_RELEASE,
 	};
 
 		
--- src/main/java/de/ingrid/importer/udk/strategy/IDCStrategyFactory.java
+++ src/main/java/de/ingrid/importer/udk/strategy/IDCStrategyFactory.java
@@ -106,6 +106,7 @@ import de.ingrid.importer.udk.strategy.v403.IDCStrategy4_0_3_b;
 import de.ingrid.importer.udk.strategy.v403.IDCStrategy4_0_3_fixKeywordsAdVMIS;
 import de.ingrid.importer.udk.strategy.v404.IDCStrategy4_0_4_a;
 import de.ingrid.importer.udk.strategy.v404.IDCStrategy4_0_4_b;
+import de.ingrid.importer.udk.strategy.v410.IDCStrategy4_1_0_RELEASE;
 
 /**
  * @author joachim
@@ -269,6 +270,8 @@ public class IDCStrategyFactory {
             return new IDCStrategy4_0_4_a();
         } else if (idcVersion.equals( IDCStrategy.VALUE_IDC_VERSION_4_0_4_b )) {
             return new IDCStrategy4_0_4_b();
+        } else if (idcVersion.equals( IDCStrategy.VALUE_IDC_VERSION_4_1_0_RELEASE )) {
+            return new IDCStrategy4_1_0_RELEASE();
         } else {
             log.error( "Unknown IDC version '" + idcVersion + "'." );
             throw new IllegalArgumentException( "Unknown IDC version '" + idcVersion + "'." );
