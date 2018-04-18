1c2970b1a329a6ad435a25e27f9dce9463fe7934
Andre Wallat <andre.wallat@wemove.com>
2018-04-18 15:44:34 +0200

    - added migration strategy depending on UVP number

--- src/main/java/de/ingrid/importer/udk/strategy/v440/IDCStrategy4_4_0_b.java
+++ src/main/java/de/ingrid/importer/udk/strategy/v440/IDCStrategy4_4_0_b.java
@@ -22,18 +22,24 @@
  */
 package de.ingrid.importer.udk.strategy.v440;
 
+import de.ingrid.codelists.model.CodeListEntry;
 import de.ingrid.importer.udk.strategy.IDCStrategyDefault;
+import de.ingrid.importer.udk.util.InitialCodeListServiceFactory;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.List;
 
 /**
  * <p>
  * Changes InGrid 4.4.0
- * <p>
+ *
  * <ul>
  * <li>migrate UVP checkbox, see
  * https://redmine.informationgrid.eu/issues/881
@@ -41,7 +47,7 @@ import java.sql.Statement;
  */
 public class IDCStrategy4_4_0_b extends IDCStrategyDefault {
 
-    private static Log log = LogFactory.getLog( IDCStrategy4_4_0_b.class );
+    private static Log log = LogFactory.getLog(IDCStrategy4_4_0_b.class);
 
     private static final String MY_VERSION = VALUE_IDC_VERSION_4_4_0_b;
 
@@ -50,21 +56,21 @@ public class IDCStrategy4_4_0_b extends IDCStrategyDefault {
     }
 
     public void execute() throws Exception {
-        jdbc.setAutoCommit( false );
+        jdbc.setAutoCommit(false);
 
-        // write version of IGC structure !
-        setGenericKey( KEY_IDC_VERSION, MY_VERSION );
+        // do not write version of IGC structure, since migration can be done multiple times !
+        // setGenericKey(KEY_IDC_VERSION, MY_VERSION);
 
         // THEN EXECUTE ALL "CREATING" DDL OPERATIONS ! NOTICE: causes commit
         // (e.g. on MySQL)
         // ---------------------------------
 
-        System.out.print( "  Migrate UVP data ..." );
+        System.out.print("  Migrate UVP data ...");
         migrateUVP();
-        System.out.println( "done." );
-        
+        System.out.println("done.");
+
         jdbc.commit();
-        System.out.println( "Update finished successfully." );
+        System.out.println("Update finished successfully.");
     }
 
     private void migrateUVP() throws Exception {
@@ -74,6 +80,17 @@ public class IDCStrategy4_4_0_b extends IDCStrategyDefault {
                         "WHERE obj_id = ? " +
                         "AND field_key = 'uvpNeedsExamination'");
 
+        PreparedStatement psUVPCategoryItems = jdbc.prepareStatement(
+                "SELECT data " +
+                        "FROM additional_field_data " +
+                        "WHERE field_key = 'categoryId' AND parent_field_id = (" +
+                        "SELECT id FROM additional_field_data " +
+                        "WHERE obj_id = ? AND field_key = 'uvpgCategory')");
+
+        // get correct UVP codelist which is defined in the catalog?
+        String uvpCodelistId = determineUvpCodelistId();
+        List<CodeListEntry> uvpCodelistEntries = InitialCodeListServiceFactory.instance().getCodeList(uvpCodelistId).getEntries();
+
         String sql = "select id from t01_object";
 
         Statement st = jdbc.createStatement();
@@ -81,20 +98,114 @@ public class IDCStrategy4_4_0_b extends IDCStrategyDefault {
         while (rs.next()) {
             long objId = rs.getLong("id");
 
-            psNeedsExamination.setLong(1, objId);
-            ResultSet resultSet = psNeedsExamination.executeQuery();
-            String valueNeedsExamination;
-            if (resultSet.next()) {
-                valueNeedsExamination = resultSet.getString("data");
+            boolean migrated = migrateByPreviousState(psNeedsExamination, objId);
 
-                if ("true".equals(valueNeedsExamination)) {
-                    log.debug("migrate UVP checkbox");
-                    sql = "INSERT INTO additional_field_data (id, obj_id, field_key, list_item_id, data) "
-                            + "VALUES (" + getNextId() + ", " + objId + ", 'uvpPreExaminationAccomplished', NULL, 'true')";
-                    jdbc.executeUpdate(sql);
+            if (!migrated) {
+                migrateByUvpNumber(uvpCodelistEntries, psUVPCategoryItems, objId);
+            }
+        }
+
+        psNeedsExamination.close();
+        psUVPCategoryItems.close();
+    }
+
+    @SuppressWarnings("unchecked")
+    private String determineUvpCodelistId() throws Exception {
+        JSONParser jsonParser = new JSONParser();
+
+        String behaviours = readGenericKey("BEHAVIOURS");
+        JSONArray json = (JSONArray) jsonParser.parse(behaviours);
+
+        // get the defined category id or return default id (9000)
+        return (String) json.stream()
+                .filter(item -> "uvpPhaseField".equals(((JSONObject) item).get("id")))
+                .findFirst()
+                .map(uvpPhaseField -> {
+                    JSONArray params = (JSONArray) ((JSONObject) uvpPhaseField).get("params");
+                    return params.stream()
+                            .filter(param -> "categoryCodelist".equals(((JSONObject) param).get("id")))
+                            .findFirst().map(categoryCodelist -> ((JSONObject) categoryCodelist).get("value"))
+                            .orElse("9000");
+                })
+                .orElse("9000"); // default codelist for UVP
+    }
+
+    /**
+     * If old checkbox "Eine Änderung/Erweiterung oder ein kumulierendes Vorhaben, für das eine Vorprüfung
+     * durchgeführt wurde" was checked, then check the new one
+     */
+    private boolean migrateByPreviousState(PreparedStatement psNeedsExamination, long objId) throws Exception {
+        psNeedsExamination.setLong(1, objId);
+        ResultSet resultSet = psNeedsExamination.executeQuery();
+        String valueNeedsExamination;
+        boolean result = false;
+
+        if (resultSet.next()) {
+            valueNeedsExamination = resultSet.getString("data");
+
+            if ("true".equals(valueNeedsExamination)) {
+                log.debug("migrate UVP checkbox");
+                tickCheckboxPreExaminationAccomplished(objId);
+                result = true;
+            }
+
+        }
+
+        resultSet.close();
+        return result;
+    }
+
+    /**
+     * If one of the UVP numbers of an element is of type "A" or "S", then check the new checkbox.
+     *
+     * @param uvpCodelistEntries contains the UVP codelist entries
+     * @param psUVPCategoryItems is the SQL statement to query the UVP numbers of a dataset
+     * @param objId              is the ID of the dataset
+     * @throws Exception gets thrown if an error occurred
+     */
+    private void migrateByUvpNumber(List<CodeListEntry> uvpCodelistEntries, PreparedStatement psUVPCategoryItems, long objId) throws Exception {
+        JSONParser jsonParser = new JSONParser();
+
+        psUVPCategoryItems.setLong(1, objId);
+        ResultSet resultSet = psUVPCategoryItems.executeQuery();
+
+        // iterate over all UVP category numbers
+        while (resultSet.next()) {
+            String catId = resultSet.getString("data");
+
+            // if an UVP number with classification "A" or "S" then tick the new checkbox
+            for (CodeListEntry entry : uvpCodelistEntries) {
+                if (entry.getId().equals(catId)) {
+
+                    // convert data field to JsonObject
+                    JSONObject json = (JSONObject) jsonParser.parse(entry.getData());
+                    String type = (String) json.get("type");
+
+                    if ("A".equals(type) || "S".equals(type)) {
+                        tickCheckboxPreExaminationAccomplished(objId);
+
+                        // one is enough
+                        resultSet.close();
+                        return;
+                    }
                 }
             }
-            resultSet.close();
         }
+        resultSet.close();
+    }
+
+    /**
+     * Set the new checkbox to checked for a dataset with the given "objId".
+     *
+     * @param objId is the ID of the dataset
+     * @throws Exception gets thrown if an error occurred
+     */
+    private void tickCheckboxPreExaminationAccomplished(long objId) throws Exception {
+        String sqlDelete = "DELETE FROM additional_field_data WHERE obj_id = " + objId + " AND field_key = 'uvpPreExaminationAccomplished'";
+        String sqlAdd = "INSERT INTO additional_field_data (id, obj_id, field_key, list_item_id, data) "
+                + "VALUES (" + getNextId() + ", " + objId + ", 'uvpPreExaminationAccomplished', NULL, 'true')";
+
+        jdbc.executeUpdate(sqlDelete);
+        jdbc.executeUpdate(sqlAdd);
     }
 }
