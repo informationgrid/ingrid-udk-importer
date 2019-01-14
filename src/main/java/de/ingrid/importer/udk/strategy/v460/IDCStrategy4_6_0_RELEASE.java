532098ba4f09bd2b432623099c42724061787e83
Martin Maidhof <martin@wemove.com>
2019-01-14 15:28:14 +0100

    Prepare Release 4.6.0 (release strategy, changes)

---
+++ src/main/java/de/ingrid/importer/udk/strategy/v460/IDCStrategy4_6_0_RELEASE.java
@@ -0,0 +1,57 @@
+/*
+ * **************************************************-
+ * InGrid UDK-IGC Importer (IGC Updater)
+ * ==================================================
+ * Copyright (C) 2014 - 2018 wemove digital solutions GmbH
+ * ==================================================
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be
+ * approved by the European Commission - subsequent versions of the
+ * EUPL (the "Licence");
+ * 
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * 
+ * http://ec.europa.eu/idabc/eupl5
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ * **************************************************#
+ */
+package de.ingrid.importer.udk.strategy.v460;
+
+import de.ingrid.importer.udk.strategy.IDCStrategyDefault;
+
+/**
+ * <p>
+ * Changes InGrid 4.6.0<p>
+ * <ul>
+ *   <li>Set database catalog version to final release version !
+ *   <li>RELOAD ALL SYSLISTS due to removing lastModifiedSyslist entry from sys_generic_key 
+ * </ul>
+ */
+public class IDCStrategy4_6_0_RELEASE extends IDCStrategyDefault {
+
+	private static final String MY_VERSION = VALUE_IDC_VERSION_4_6_0_RELEASE;
+
+	public String getIDCVersion() {
+		return MY_VERSION;
+	}
+
+	public void execute() throws Exception {
+		jdbc.setAutoCommit(false);
+
+		// write version of IGC structure !
+		setGenericKey(KEY_IDC_VERSION, MY_VERSION);
+
+		// delete time stamp of last update of syslists to reload all syslists
+		// (reload from initial codelist file from codelist service if no repo connected).
+		// Thus we guarantee syslists are up to date !
+		deleteGenericKey("lastModifiedSyslist");
+
+		jdbc.commit();
+		System.out.println("Update finished successfully.");
+	}
+}
