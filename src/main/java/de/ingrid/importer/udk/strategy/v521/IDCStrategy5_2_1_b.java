0d5db05000d49a5c93311a0edf3ff1a430195e78
André Wallat <andre.wallat@wemove.com>
2019-11-12 11:52:55 +0100

    Add data migration

--- src/main/java/de/ingrid/importer/udk/strategy/v521/IDCStrategy5_2_1_b.java
+++ src/main/java/de/ingrid/importer/udk/strategy/v521/IDCStrategy5_2_1_b.java
@@ -22,8 +22,11 @@
  */
 package de.ingrid.importer.udk.strategy.v521;
 
+import de.ingrid.codelists.model.CodeList;
+import de.ingrid.codelists.model.CodeListEntry;
 import de.ingrid.importer.udk.jdbc.DBLogic;
 import de.ingrid.importer.udk.strategy.IDCStrategyDefault;
+import de.ingrid.importer.udk.util.InitialCodeListServiceFactory;
 import de.ingrid.utils.ige.profile.MdekProfileUtils;
 import de.ingrid.utils.ige.profile.ProfileMapper;
 import de.ingrid.utils.ige.profile.beans.ProfileBean;
@@ -32,6 +35,9 @@ import de.ingrid.utils.ige.profile.beans.controls.Controls;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -44,7 +50,9 @@ public class IDCStrategy5_2_1_b extends IDCStrategyDefault {
 
     private static Log log = LogFactory.getLog(IDCStrategy5_2_1_b.class);
 
-    private static final String MY_VERSION = VALUE_IDC_VERSION_5_2_1_b;
+    private static final String MY_VERSION = VALUE_IDC_VERSION_5_2_1_c;
+
+    CodeList codeList6300 = InitialCodeListServiceFactory.instance().getCodeList(Integer.toString(6300));
 
     public String getIDCVersion() {
         // Returning version here enables strategy workflow !
@@ -69,10 +77,13 @@ public class IDCStrategy5_2_1_b extends IDCStrategyDefault {
         // THEN PERFORM DATA MANIPULATIONS !
         // ---------------------------------
 
-        System.out.print("Add new field 'date' ...");
+        log.info("Add new field 'date' ...");
+        addDateColumn();
 
-        // addDateColumn();
+        log.info("Migrate data ...");
+        migrateData();
 
+        log.info("Update Profile ...");
         // read profile
         String profileXml = readGenericKey(KEY_PROFILE_XML);
         if (profileXml == null) {
@@ -95,6 +106,86 @@ public class IDCStrategy5_2_1_b extends IDCStrategyDefault {
         System.out.println("Update finished successfully.");
     }
 
+    private void migrateData() throws Exception {
+
+        handleGMLEntry();
+        handleApplicationSchemaDate();
+
+    }
+
+    private void handleGMLEntry() throws Exception {
+
+        PreparedStatement psClasses1AndInspire = jdbc
+                .prepareStatement("SELECT * FROM t01_object WHERE obj_class='1' AND is_inspire_relevant='Y'");
+        PreparedStatement psHasGMLEntry = jdbc
+                .prepareStatement("SELECT * FROM t0110_avail_format WHERE obj_id=? AND format_key=14");
+        PreparedStatement psAddGMLEntry = jdbc
+                .prepareStatement("INSERT INTO t0110_avail_format (id, version, obj_id, line, format_value, format_key, ver, file_decompression_technique, specification) VALUES (?, 0, ?, 1, 'GML', 14, '3.2', null, null)");
+        PreparedStatement psUpdateGMLVersion = jdbc
+                .prepareStatement("UPDATE t0110_avail_format SET ver='3.2' WHERE obj_id=? AND format_key=14");
+
+        // check if GML with version exists for INSPIRE relevant datasets
+        ResultSet inspireResult = psClasses1AndInspire.executeQuery();
+        while (inspireResult.next()) {
+            long objId = inspireResult.getLong("id");
+            psHasGMLEntry.setLong(1, objId);
+            ResultSet gmlEntries = psHasGMLEntry.executeQuery();
+            boolean hasGML = false;
+            while (gmlEntries.next()) {
+                hasGML = true;
+                String version = gmlEntries.getString("ver");
+                if (version == null || version.isEmpty()) {
+                    // update version
+                    log.info("Add version to existing GML entry for objId: " + objId);
+                    psUpdateGMLVersion.setLong(1, objId);
+                    psUpdateGMLVersion.executeUpdate();
+                }
+            }
+            if (!hasGML) {
+                log.info("Add new GML entry for objId: " + objId);
+                psAddGMLEntry.setLong(1, getNextId());
+                psAddGMLEntry.setLong(2, objId);
+                psAddGMLEntry.execute();
+            }
+        }
+        psClasses1AndInspire.close();
+        psHasGMLEntry.close();
+        psAddGMLEntry.close();
+        psUpdateGMLVersion.close();
+
+    }
+
+    private void handleApplicationSchemaDate() throws SQLException {
+
+        PreparedStatement psFormatInspire = jdbc.prepareStatement("SELECT * FROM object_format_inspire");
+        PreparedStatement psUpdateFormatInspire = jdbc.prepareStatement("UPDATE object_format_inspire SET date = ? WHERE obj_id = ?");
+
+        ResultSet resultSet = psFormatInspire.executeQuery();
+        while (resultSet.next()) {
+            int format_key = resultSet.getInt("format_key");
+            Date date = resultSet.getDate("date");
+            if (format_key != -1 && date == null) {
+                long objId = resultSet.getLong("obj_id");
+                psUpdateFormatInspire.setString(1, mapFormatKeyToDate(String.valueOf(format_key)));
+                psUpdateFormatInspire.setLong(2, objId);
+                psUpdateFormatInspire.executeUpdate();
+            }
+        }
+
+        psFormatInspire.close();
+        psUpdateFormatInspire.close();
+
+    }
+
+    private String mapFormatKeyToDate(String key) {
+        for (CodeListEntry entry : codeList6300.getEntries()) {
+            if (entry.getId().equalsIgnoreCase(key)) {
+                return entry.getData().split("\",\"")[2];
+            }
+        }
+        return null;
+    }
+
     private void addFieldToProfile(ProfileBean profileBean) {
         Rubric rubric = MdekProfileUtils.findRubric(profileBean, "availability");
         Integer uiElement1315Index = MdekProfileUtils.findControlIndex(profileBean, rubric, "uiElement1315");
@@ -109,7 +200,6 @@ public class IDCStrategy5_2_1_b extends IDCStrategyDefault {
     }
 
     private void makeFieldOptional(ProfileBean profileBean) {
-
 
         Controls control = MdekProfileUtils.findControl(profileBean, "uiElement1315");
         control.setIsMandatory(false);
